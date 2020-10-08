5bba8b28405e66508c25707201d9e5fd0bc16607
Andrew_Tud <tyjhbk2njobuxwcbbt6i64uptqwf5jemappk2vpmhukftonvgovq>
2020-10-08 16:44:02 +0800

    First Commit

---
+++ TicTacToe/TicTacToe.API/Controllers/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TicTacToe.Business.Entities;
+using TicTacToe.Business.Interface;
+
+namespace TicTacToe.API.Controllers
+{
+    public class GameController : ApiController
+    {
+        private readonly IGame _iGame;
+        public GameController(IGame iGame)
+        {
+            _iGame = iGame;
+        }
+
+        // POST api/values
+        public Player Post([FromBody] List<Position> positions)
+        {
+            return _iGame.Play(positions);
+        }
+    }
+}
